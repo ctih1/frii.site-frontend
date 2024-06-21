cba422160c47c1d6962b3cdb6f8e8585f6025cea
ctih <78687256+ctih1@users.noreply.github.com>
2024-06-21 11:12:08 +0200

    new homepage (#33)
    Co-authored-by: ctih <nevalaonni@proton.me>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,91 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import Holder from "$lib/components/Holder.svelte";
-    let videoContainer:HTMLDivElement;
-
+    import BubbleBackground from "$lib/components/BubbleBackground.svelte";
+    import Button from "$lib/components/Button.svelte";
 </script>
 <svelte:head>
     <title>frii.site</title>
 </svelte:head>
 
+<BubbleBackground>
 <div class="content">
-    <div bind:this={videoContainer} class="video-container" >
-        <video class="foreground"
-            poster="https://s4hype.xyz/assets/thumbnail_fs.webp"
-            autoplay
-            loop
-            preload="metadata"
-            muted
-            src="https://s4hype.xyz/assets/fsstory.mp4"
-            type="video/mp4"
-        />
+    <div class="parent">
+        <div class="div1"> 
+            <div class="text-container">
+                <h1pp><strong>frii.site</strong></h1pp>
+                <p style="font-size: 1.5em;"><i>Go get your subdomain now!</i></p>
+            </div>
+        </div>
+
+        <div class="div2"> 
+            <div class="div2-content">
+                <div class="button-holder">
+                    <x class="bh"><Button args={"fill"}>Register an account</Button></x>
+                    <x class="bh"><Button args={"fill"}>Go to the Dashboard</Button></x>
+                </div>  
+            </div>  
+        </div>
+
+        <div class="div3">
+    
+        </div>
+
+        <div class="div4">
+
+        </div>
     </div>
 </div>
-
-<Holder>
-    <h1p>Get started!</h1p>
-    <h2>To get started, please <a href="/account">make an account</a>.</h2>
-    
-    <div id="how-it-works">
-        <h1>How it works</h1>
-        <ul>
-            <li>You <a href="/account">register</a> an account</li>
-            <li>You <a href="/dashboard">register a domain</a></li>
-            <li>Our servers register the domain as yours, which means you are the only one that can access it</li>
-            <li>You have full access to the domain in <a href="/dashboard">the dashboard</a></li>
-        </ul>
-    </div>
-</Holder>
-
-
+</BubbleBackground>
 <style>
-    h1p {
-        font-size: 3em;
-        font-weight: 600;
+    .parent {
+        display: grid;
+        grid-template-columns: 3fr 2fr;
+        grid-template-rows: 1fr 5fr 1fr;
+        grid-column-gap: 0px;
+        grid-row-gap: 0px;
+        width: 100vw;
+    }
+
+    .div1 {
+        grid-area: 2 / 1 / 3 / 2;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+    }
+    .text-container {
+        margin-top: auto;
+        margin-bottom: auto;
+    }
+    .text-container * {
+        height: fit-content;
+        margin-top: 0px;
+        margin-left: 0.5em;
+    }
+    .button-holder {
+        display: flex;
+        flex-direction: row;
+    }
+    .button-holder x {
+        margin: 2px;
+        display: flex;
+        width: 10vw;
+        height: 2vw;
+        min-width: 200px;
+        min-height: 50px;
+    }
+    .div2-content {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .div2 {
+        display: flex;
+        grid-area: 2 / 2 / 3 / 3; 
+    }
+    .div3 { grid-area: 1 / 1 / 2 / 3; }
+    .div4 { grid-area: 3 / 1 / 4 / 3; }
+
+    h1pp {
+        font-size: 10em;
     }
     .content {
         display: flex;
@@ -49,27 +93,70 @@
         width: 100vw;
         height: 100vh ;
     }
-    .right {
-        width: 40vw;
+
+    @media(max-width: 1330px) {
+        .text-container h1pp strong, .text-container h1pp{
+            margin-left: 20px;
+        }
+        .text-container {
+            text-align: left;
+        }
+        .text-container p {
+            margin-left: 30px;
+        }
+
+    } 
+    @media(max-width: 1030px) {
+        h1pp {
+            font-size: 8em;
+        }
     }
-    .video-container {
-        aspect-ratio: 1;
-        width: 100vw;
-        z-index: 99;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        overflow:hidden;
+    @media (orientation: portrait), (max-width: 745px) {
+        .parent {
+            margin-top: auto;
+            margin-bottom: auto;
+            grid-template-columns: 1fr;
+            grid-template-rows: auto;
+        } 
+        .parent * {
+            align-items: center;
+            margin-right: auto;
+            margin-left: auto;
+        }
+        .button-holder x {
+            width: 48vw;
+            height: 4em;
+        }
+        .parent div {
+            grid-area: auto/auto/auto/auto;
+            width: 100vw;
+            height: fit-content;
+        }
+        .parent div * {
+            margin-left: auto;
+            margin-right: auto;
+        }
+        h1pp {
+            font-size: 14em;
+        }
+        .text-container {
+            text-align: center;
+        }
     }
-    .foreground {
-        object-fit: cover;
-        z-index: 3;
-        width: 100vw;
-        height: 120vh;
-        margin-left: auto;
-        margin-right: auto;
+    @media(max-width: 795px) {
+        h1pp {
+            font-size: 12em;
+        }
     }
-    li {
-        font-size: 1.25em;
+    @media(max-width: 690px) {
+        h1pp {
+            font-size: 10em;
+        }
     }
+    @media(max-width: 580px) {
+        h1pp {
+            font-size: 8em;
+        }
+    }
+
 </style>
