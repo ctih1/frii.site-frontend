b8ca29584c2145c6645d14ec5f394f3890b08ace
Onni Nevala <nevalaonni@gmail.com>
2024-07-13 18:08:12 +0300

    More debugging I want to kill myself why the fuck is this not working

--- src/routes/report/manage/[slug]/+page.ts
+++ src/routes/report/manage/[slug]/+page.ts
@@ -2,10 +2,10 @@ import { serverURL } from '../../../../serverContactor';
 
 export async function load({ params }){
     let code=params.slug;
-
+    await fetch(`${serverURL}`).then(response=>{console.log(response.status)});
     const dataRequest = await fetch(`${serverURL}/vulnerability/get`,{
         method:"POST",
-        headers:{"Content-type":"application/x-www-form-urlencoded"},
+        headers:{"Content-type":"application/json"},
         body: JSON.stringify({"id":code})
     });
     const data = await dataRequest.json();
