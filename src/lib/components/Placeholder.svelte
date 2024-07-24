76f9ceb5711beb0e864c5e20850a6968014c5cc0
ctih <78687256+ctih1@users.noreply.github.com>
2024-07-24 15:55:49 +0300

    Dev (#104)

--- src/lib/components/Placeholder.svelte
+++ src/lib/components/Placeholder.svelte
@@ -25,7 +25,7 @@
         animation-timing-function: linear;
         background: #f6f7f8;
         background: linear-gradient(to right, #eeeeee 8%, #dddddd 18%, #eeeeee 33%);
-        background-size: 1000px 100%;
+        background-size: 500% 100%;
         overflow: hidden;
         border-radius: 0.5em;
     }
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,7 @@
 import { prefLocale } from './stores.js';
 import { get } from "svelte/store"
 import { addTranslations, setLocale, setRoute } from '$lib/translations';
-
+export const ssr = false;
 /** @type {import('@sveltejs/kit').Load} */
 export const load = async ({ data }) => {
   const { i18n, translations } = data;
