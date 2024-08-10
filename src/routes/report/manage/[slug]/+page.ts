84749c4fa912c97a5ebae5d64b825598b31935a7
ctih <78687256+ctih1@users.noreply.github.com>
2024-08-10 19:22:22 +0300

    changed vulnerbility tracking method

--- src/routes/report/manage/[slug]/+page.ts
+++ src/routes/report/manage/[slug]/+page.ts
@@ -3,10 +3,8 @@ import { serverURL } from '../../../../serverContactor';
 export async function load({ params }){
     let code=params.slug;
     await fetch(`${serverURL}`).then(response=>{console.log(response.status)});
-    const dataRequest = await fetch(`${serverURL}/vulnerability/get`,{
-        method:"POST",
-        headers:{"Content-type":"application/json"},
-        body: JSON.stringify({"id":code})
+    const dataRequest = await fetch(`${serverURL}/vulnerability/get?id=${code}`,{
+        method:"GET",
     });
     const data = await dataRequest.json();
     return data;
--- src/routes/report/track/+page.ts
+++ src/routes/report/track/+page.ts
@@ -3,11 +3,10 @@ import { serverURL } from '../../../serverContactor';
 export async function load({ url }) {
     let code:string|null=url.searchParams.get("code");
     await fetch(`${serverURL}`).then(response=>{console.log(response.status)});
-    const dataRequest = await fetch(`${serverURL}/vulnerability/get`,{
-        method:"POST",
-        headers:{"Content-Type":"application/json"},
-        body: JSON.stringify({"id":code})
+    const dataRequest = await fetch(`${serverURL}/vulnerability/get?id=${code}`,{
+        method:"GET",
     });
     const data = await dataRequest.json();
     return data;
 }  
+
