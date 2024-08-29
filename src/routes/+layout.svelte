fdeedafa53d78d5a0fe7a3b9980873bff5ab100a
ctih <78687256+ctih1@users.noreply.github.com>
2024-08-29 18:01:37 +0300

    Dev (#153)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,41 +2,39 @@
     import Header from "$lib/components/Header.svelte";
     import Analytics from "$lib/components/Analytics.svelte";
     import Banner from "$lib/components/Banner.svelte";
-    import Ads from "$lib/components/Ads.svelte"
+    import Ads from "$lib/components/Ads.svelte";
 </script>
 
-<Banner/>
-<Header/>
-<Analytics/>
-<Ads></Ads>
-<svelte:head>
+<Banner />
+<Header />
+<Analytics />
 
-</svelte:head>
+<Ads></Ads>
+<svelte:head></svelte:head>
 <main>
-    <slot/>
+    <slot />
 </main>
 
 <style>
     :root {
         --primary: rgb(0, 123, 225);
         --border-color: rgba(0, 0, 0, 0.086);
-        --border-color: rgba(0,0,0,0.05);
+        --border-color: rgba(0, 0, 0, 0.05);
         --secondary-color: #bdbdbd;
         --offwhite-color: #eeeeee;
         --background-color: rgb(216, 216, 216);
-        overflow-x: hidden; 
-        
+        overflow-x: hidden;
     }
-    :root::-webkit-scrollbar{
+    :root::-webkit-scrollbar {
         display: none;
     }
-    :root{
+    :root {
         -ms-overflow-style: none;
         scrollbar-width: none;
     }
-    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+    @import url("https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap");
     :global(*) {
-        font-family: "Inter",sans-serif;
+        font-family: "Inter", sans-serif;
     }
     :global(input) {
         transition: all 0.1s;
@@ -44,11 +42,11 @@
         border-color: var(--border-color);
         border-radius: 0.5em;
         width: 100%;
-        height:100%;
+        height: 100%;
         box-sizing: border-box;
         outline-color: var(--primary);
     }
-    :global(input[type=checkbox]) {
+    :global(input[type="checkbox"]) {
         transition: all 0.3s;
         width: 100%;
         max-width: 2em;
@@ -61,7 +59,7 @@
         border-color: var(--border-color);
         border-radius: 0.5em;
         width: 100%;
-        height:100%;
+        height: 100%;
         box-sizing: border-box;
         outline-color: var(--primary);
         resize: vertical;
@@ -72,11 +70,9 @@
         font-size: inherit;
     }
     :global(.holder) {
-
     }
-    
 
-    @media (min-width:960px) {
+    @media (min-width: 960px) {
         .holder {
             width: 100vw;
             margin-left: 2em;
