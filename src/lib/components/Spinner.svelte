446ca4af9e330b1ad8c38f8e5ca4ab3db33a7138
Onni Nevala <nevalaonni@Onni-MacBook-Pro.local>
2024-09-01 17:06:16 +0300

    added a spinner and a better loading indicator

---
+++ src/lib/components/Spinner.svelte
@@ -0,0 +1,26 @@
+<div class="spinner"></div>
+
+<style>
+    .spinner {
+        border: 0.5em solid var(--secondary-color);
+        border-top: 0.5em solid var(--primary);
+        border-radius: 50%;
+        aspect-ratio: 1;
+        width: 50px;
+        animation: spin 1.5s linear infinite forwards;
+    }
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        45% {
+            transform: rotate(360deg);
+        }
+        80% {
+            transform: rotate(900deg);
+        }
+        100% {
+            transform: rotate(1080deg);
+        }
+    }
+</style>
