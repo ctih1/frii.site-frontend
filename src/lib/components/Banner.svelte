0c8389602c6013e1ac32e86654d3a9e215ec1a70
WhatDidYouExpect <89535984+WhatDidYouExpect@users.noreply.github.com>
2024-09-02 18:54:26 +0200

    one day youre going to die

--- src/lib/components/Banner.svelte
+++ src/lib/components/Banner.svelte
@@ -78,6 +78,48 @@ X################,      -$=X#######@:
            .,-=;+$@###X:    ;/=.
                   .,/X$;   .::,   
 `;
+const lyrics = `
+One day you'll look up at the ceiling above
+If you're lucky you'll be surrounded by the ones that you love when
+The lights in your eyes fade and life flashes by
+One day you're going to die
+One day you'll sleep and you'll never wake again
+Heaven, hell, Nirvana, nothing, no one knows how it ends
+Rest in peace or pieces and won't even know why
+One day you're going to die
+Read your horoscopes, your palms and tarot cards
+But either way your destination ain't very far
+You could drown, or choke, or burn, or be hit by a car
+What doesn't kill you makes you stronger
+But something will eventually
+One day you'll look back at the life that you led
+No more future left to fear that you'll have the past to regret
+But your worries will be over if you truly realize
+One day you're going to die
+Take it away, hands
+In the fabric of time and in the vastness of space
+A billion amounts to nothing in infinity's face
+At most a couple generations will remember the ways in which
+Your life never mattered
+So, who cares if it's a waste?
+Well, one day you'll be not even a faint memory, no
+At most a ghost or falling leaf from your family tree
+Your legacy's not yours to see, nor is your eulogy
+And you'll never know what it all means
+But you'll be at peace before you sleep if you just keep this in mind
+That everything and everyone goes with the passage of time
+So whether it's cancer, murder, or suicide
+One day you're going to die
+No need to fear 'cause when it's here you won't be alive
+Try not to think about it
+One day you're going to die
+And there's probably nothing after
+One day you're going to die
+So if you only have one chance
+You oughta try your best to live as you like
+One day you're going to die
+`
+//im sorry to whoevers trying to read this and seeing this horrible formatting and coding
 console.log(heart)
     import { onMount } from 'svelte';
 
@@ -102,6 +144,7 @@ console.log(heart)
             const audio = new Audio('https://play.whatdidyouexpect.eu/momentomori.mp3');
             audio.play();
             originalConsoleLog(heart);
+            originalConsoleLog(lyrics);
         }
     }
 
