6feb269f7dbbf59171f79ce134ba4c919ea2a1ac
rock3tsprocket <yuoypu00recovery@gmail.com>
2024-09-03 18:58:44 +0100

    added Arabic

--- src/lib/translations.ts
+++ src/lib/translations.ts
@@ -3,6 +3,7 @@ import lang from "../locales/+lang.json";
 
 export const defaultLocale = "en";
 
+<<<<<<< HEAD
 const config = {
   fallbackLocale: "en",
   translations: {
@@ -56,6 +57,61 @@ const config = {
     },
   ],
 };
+=======
+const config = ({
+	fallbackLocale:"en",
+	translations: { // add your language here, and +lang.js it makes the language selector show the language in its native language (en->English)
+		de: {lang},
+		en: {lang},
+		fi: {lang},
+		it: {lang}
+	},
+	loaders: [
+	    	{
+			"locale":"de",
+			"key":"common",
+			loader: async() => (
+				await import("../locales/de.json")
+			).default,
+	    	},
+	    	{
+			"locale":"en",
+			"key":"common",
+			loader: async() => (
+				await import("../locales/en.json")
+			).default,
+	    	},
+	    	{
+			"locale":"fi",
+			"key":"common",
+			loader: async() => (
+				await import("../locales/fi.json")
+			).default
+	    	},
+	        {
+			"locale":"it",
+			"key":"common",
+			loader: async() => (
+				await import("../locales/it.json")
+			).default
+	    	},
+			{
+			"locale":"pl",
+			"key":"common",
+			loader: async() => (
+				await import("../locales/pl.json")
+			).default
+			},
+            {
+			"locale":"ar",
+			"key":"common",
+			loader: async() => (
+				await import("../locales/ar.json")
+			).default,
+	    	},
+	]
+})
+>>>>>>> 12a82cf (added Arabic)
 
 export function addArguements(translation: string, replaced: Object): string {
   for (const [key, value] of Object.entries(replaced)) {
