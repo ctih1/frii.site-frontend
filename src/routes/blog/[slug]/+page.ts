a8ca289eca97b467fbcc06cade95ff59d5f86349
WhatDidYouExpect <89535984+WhatDidYouExpect@users.noreply.github.com>
2024-09-04 17:40:34 +0200

    look at me. i control the blogs now.

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -8,7 +8,7 @@ export async function load({ params }) {
     body: string;
   }
   let articleName = params.slug;
-  const dataRequest = await fetch(`${serverURL}/blog/${articleName}`, {
+  const dataRequest = await fetch(`https://frii-site-emulator.vercel.app/blog/${articleName}`, {
     method: "GET",
   });
   let data: article;
