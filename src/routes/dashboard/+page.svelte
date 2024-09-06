7e6b50318eef2bf385acd47034f36a55ee2e735a
ctih <78687256+ctih1@users.noreply.github.com>
2024-09-06 23:43:39 +0300

    I/loader (#165)
    * added a loader for the domain dashboard
    
    * added more loader imlementation
    
    * added more translations and fixed loader never disappearing
    
    ---------
    
    Co-authored-by: ctih1 <contact@frii.site>

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,33 +1,32 @@
 <script lang="ts">
-	import Blur from '$lib/components/Blur.svelte';
 	import Registrar from '$lib/components/Registrar.svelte';
     import DomainTable from "$lib/components/DomainTable.svelte";
     import Modal from "$lib/components/Modal.svelte";
     import Holder from '$lib/components/Holder.svelte';
+    import Loader from "$lib/components/Loader.svelte";
     import { t,l,locale,addArguements } from '$lib/translations';
     import { ServerContactor } from '../../serverContactor';
     import { onMount } from 'svelte';
     import { redirectToLogin } from '../../helperFuncs';
-    import { redirect } from '@sveltejs/kit';
 
-    let blurBackground:Blur;
     let domainTable:DomainTable;
     let modal:Modal;
     let domains:Map<any,any>;
     let domainlist:Array<Array<string>> = [];
     let serverContactor:ServerContactor
     let domain2delete:string;
-
+    let loader:Loader;
     let responseSave:Response;
     function modalClose() {
         modal.close();
-        blurBackground.hide();
     }
 
     let modalTime:number = 15;
 
     function modalConfirm() {
+      loader.show(undefined,addArguements($t("common.dashboard_delete_loading_desc"),{"%domain%":domain2delete}))
         serverContactor.deleteDomain(domain2delete).then(response=>{
+          loader.hide();
             switch(response.status) {
                 case 200:
                     modal.open(addArguements($t("common.dashboard_delete_success"),{"%domain%":domain2delete}),addArguements($t("common.dashboard_delete_success_description"),{"%domain%":domain2delete}));
@@ -41,9 +40,10 @@
     }
 
     function registerDomain(domain:string,type:string) {
-        blurBackground.show();
+        loader.show($t("common.loading"), addArguements($t("common.dashboard_register_load_desc"),{"%domain%":domain}));
         serverContactor.registerDomain(domain,type).then(response=>{
             const errorMessage = addArguements($t("common.dashboard_register_fail"),{"%domain%":domain})
+            loader.hide();
             switch(response.status) {
                 case 200:
                     modal.open(addArguements($t("common.dashboard_register_success"),{"%domain%":domain}),$t("common.dashboard_register_success_description"));
@@ -76,9 +76,10 @@
     }
 
     function modifyDomain(name:string,value:string,type:string) {
-        blurBackground.show();
+        loader.show($t("common.loading"),addArguements($t("common.dashboard_modify_load_desc"),{"%domain%":name}));
         serverContactor.modifyDomain(name,value,type).then(response=>{
             const errorMessage = addArguements($t("common.dashboard_modify_fail"),{"%domain%":name})
+            loader.hide();
             switch(response.status) {
                 case 401:
                     redirectToLogin(401);
@@ -103,7 +104,7 @@
                     break;
             }
         })
-    } 
+    }
 
     function removeDomain(name:string) {
         domainlist=domainlist.filter(function(domain) {
@@ -114,7 +115,7 @@
 
 onMount(()=>{
         // stupid typescript done got fooled by the simplest trick in the book
-        if(localStorage.getItem("del-count")??null===true) { 
+        if(localStorage.getItem("del-count")??null===true) {
             modalTime = 3;
         }
         serverContactor = new ServerContactor(localStorage.getItem("auth-token"),localStorage.getItem("server_url"));
@@ -146,6 +147,8 @@ onMount(()=>{
     <meta content="#007be1" data-react-helmet="true" name="theme-color" />
 </svelte:head>
 
+<Loader bind:this={loader}/>
+
 <Holder>
     <h1>{$t("common.dashboard_your_domains")}</h1>
     <p>{$t("common.dashboard_domain_explanation")}</p>
@@ -161,7 +164,6 @@ onMount(()=>{
 </Holder>
 
 <Modal overrideDefault={true} on:primary={()=>modalClose()} on:secondary={()=>modalConfirm()} bind:this={modal} options={[$t("common.modal_ok")]} description={""} title={""}></Modal>
-<Blur reverse={false} bind:this={blurBackground}/>
 <style>
 
 </style>
