5871f7c4bd20fe781731df192616e47d216277f3
ctih <78687256+ctih1@users.noreply.github.com>
2024-09-07 16:25:31 +0300

    added loader on account page, and made blog embeds use the title (#169)
    Co-authored-by: ctih1 <contact@frii.site>

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -12,6 +12,10 @@
     time.setSeconds(data.date);
 </script>
 
+<svelte:head>
+    <meta content={`${data.title} - frii.site`}>
+</svelte:head>
+
 <link
     rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
