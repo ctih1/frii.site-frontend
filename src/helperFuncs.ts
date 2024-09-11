6702f4b7324b7312c3f5a0562476a7671ffbe56d
ctih <78687256+ctih1@users.noreply.github.com>
2024-09-11 18:07:59 +0300

    Dev (#172)
    * improved translation feature
    
    * Update README.md
    
    * added Arabic
    
    * actually added Arabic
    
    * fix typos
    
    * I/loader (#165)
    
    * added a loader for the domain dashboard
    
    * added more loader imlementation
    
    * added more translations and fixed loader never disappearing
    
    ---------
    
    Co-authored-by: ctih1 <contact@frii.site>
    
    * added romanian and fixed formatting on translation.ts
    
    * added bulgarian translation
    
    * added credit for bulgarian and romanian translation
    
    * added edge case for arabic
    
    * I/tpicker (#168)
    
    * added romanian and fixed formatting on translation.ts
    
    * added bulgarian translation
    
    * added credit for bulgarian and romanian translation
    
    * added edge case for arabic
    
    * added sorting function to sort languages by names
    
    ---------
    
    Co-authored-by: ctih1 <contact@frii.site>
    
    * added loader on account page, and made blog embeds use the title (#169)
    
    Co-authored-by: ctih1 <contact@frii.site>
    
    * I/store (#170)
    
    * added loader on account page, and made blog embeds use the title
    
    * Improved store page, and added translations for it
    
    ---------
    
    Co-authored-by: ctih1 <contact@frii.site>
    
    * fixed loader not being hidden once logged in (#171)
    
    Co-authored-by: ctih1 <contact@frii.site>
    
    ---------
    
    Co-authored-by: Onni Nevala <nevalaonni@Onni-MacBook-Pro.local>
    Co-authored-by: Rock Pie <139387589+rock3tsprocket@users.noreply.github.com>
    Co-authored-by: rock3tsprocket <yuoypu00recovery@gmail.com>
    Co-authored-by: nikolan <94351312+nikolan123@users.noreply.github.com>
    Co-authored-by: ctih1 <contact@frii.site>

--- src/helperFuncs.ts
+++ src/helperFuncs.ts
@@ -1,16 +1,27 @@
-export function redirectToLogin(code:number=0):void {
-    localStorage.removeItem("logged-in");
-    window.location.href=`/account?r=${window.location.pathname}&c=${code}`
+export function redirectToLogin(code: number = 0): void {
+  localStorage.removeItem("logged-in");
+  window.location.href = `/account?r=${window.location.pathname}&c=${code}`;
 }
-export function createFile(filename:string,content:string):boolean {
-    let a:HTMLElement=document.createElement("a");
-    a.setAttribute("href","data:text/plain;charset=utf-8,"+encodeURIComponent(content));
-    a.setAttribute("download",filename);
-    a.click();
-    return true;
+export function createFile(filename: string, content: string): boolean {
+  let a: HTMLElement = document.createElement("a");
+  a.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(content),
+  );
+  a.setAttribute("download", filename);
+  a.click();
+  return true;
 }
-export function getFlagEmoji (countryCode:String):string {
-    if(countryCode=="en") {countryCode="gb"};
-	let codePoints = countryCode.toUpperCase().split('').map(char =>  127397 + char.charCodeAt());
-	return String.fromCodePoint(...codePoints);
+export function getFlagEmoji(countryCode: String): string {
+  if (countryCode == "en") {
+    countryCode = "gb";
+  }
+  if (countryCode == "ar") {
+    countryCode = "sa";
+  }
+  let codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
 }
