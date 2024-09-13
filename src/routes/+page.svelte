4be8bd50b3528f506ba43670467910e65b372d01
ctih1 <contact@frii.site>
2024-09-14 00:18:58 +0300

    added basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,246 +5,99 @@
     import BubbleBackground from "$lib/components/BubbleBackground.svelte";
     import Button from "$lib/components/Button.svelte";
 </script>
-<svelte:head>   
+<svelte:head>
     <title>frii.site</title>
 </svelte:head>
-
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 <BubbleBackground>
-<div class="content">
-    <div class="parent">
-        <div class="div1"> 
-            <div class="text-container">
-                <h1><strong>frii.site</strong></h1>
-                <p style="font-size: 1.5em;"><i>{ $t("common.index_description") }</i></p>
-            </div>
+    <h1 class="title" style="font-size: 10em;">frii.site</h1>
+    <div class="introduction-wrapper">
+        <div class="center">
+            <h1 style="font-size: 5em; margin-bottom: 0px;"><strong>{$t("common.index_aboutus")}</strong></h1>
+            <hr style="width: 100%; opacity: 0.1; height: 4px;"/>
+            <p style="margin-top: 10px;">{@html $t("common.index_aboutus_description")}
         </div>
-
-        <div class="div2"> 
-            <div class="div2-content">
-                <div class="button-holder">
-                    <x class="bh"><Button on:click={()=>{window.location.href="/account";}} args={"fill"}><a style="color: #ffffff;" href="/account">{$t("common.index_register_account")}</a></Button></x>
-                    <x class="bh"><Button on:click={()=>{window.location.href="/dashboard";}} args={"fill"}><a style="color: #ffffff;" href="/dashboard">{$t("common.index_goto_dashboard")}</a></Button></x>
-                </div>  
-            </div>  
+        <div class="left">
+            <h2><span class="material-symbols-outlined">lock</span>Security</h2>
         </div>
-
-        <div class="div3">
-    
+        <div class="right">
+            <h2><span class="material-symbols-outlined">raven</span>Freedom</h2>
         </div>
-
-        <div class="div4">
-
+        <div class="bottom-left">
+            <h2><span class="material-symbols-outlined">support</span>Support</h2>
+        </div>
+        <div class="bottom-right">
+            <h2><span class="material-symbols-outlined">encrypted</span>Privacy</h2>
         </div>
     </div>
-</div>
-<div class="content">
-    <div class="parent">
-        <div id="about-us" class="div1"> 
-            <div class="text-container">
-                <h2><strong>{$t("common.index_aboutus")}</strong></h2>
-            </div>
-        </div>
-
-        <div class="div2"> 
-            <div class="div2-content">
-                <h3 style="height: fit-content;" class="info">{@html $t("common.index_aboutus_description")}</h3>
-            </div>  
-        </div>
-
-    </div>
-</div>
 </BubbleBackground>
-<style>
-    .parent {
-        display: grid;
-        grid-template-columns: 3fr 2fr;
-        grid-template-rows: 1fr 5fr 1fr;
-        grid-column-gap: 0px;
-        grid-row-gap: 0px;
-        width: 100vw;
-    }
 
-    .div1 {
-        grid-area: 2 / 1 / 3 / 2;
-        text-align: center;
+<style>
+    .title {
+        padding: 10px;
+        transition: all 0.3s;
+        background: radial-gradient(circle, rgb(80,120,225), rgb(100,120,225));
+        -webkit-background-clip: text;
+        background-clip: text;
+        color: transparent;
+        animation: textHue 7s linear infinite forwards;
+        background-size: 400% 100%;
+    }
+    .title:hover {
+
+    }
+    .introduction-wrapper {
+        display: grid;
+        width: 100vw;
+        height: 100vh;
+        grid-template-rows: repeat(3,1fr);
+        grid-template-columns: repeat(3,1fr);
+    }
+    .introduction-wrapper div {
         display: flex;
         flex-direction: column;
-    }
-    
-    .text-container {
-        margin-top: auto;
-        margin-bottom: auto;
-    }
-    .text-container * {
-        height: fit-content;
-        margin-top: 0px;
-        margin-left: 0.5em;
-    }
-    .button-holder {
-        display: flex;
-        flex-direction: row;
-        margin-right:auto;
-        height: 15%;
-        width: 60%;
-    }
-    .button-holder x {
-        margin: 2px;
-        display: flex;
-        width: 45%;
-        height: 100%;
-        min-width: 80px;
-        min-height: 50px;
-    }
-    .div2-content {
-        width: 100%;
-        height: 100%;
-        display: flex;
         align-items: center;
         justify-content: center;
     }
-    .div2 {
-        display: flex;
-        grid-area: 2 / 2 / 3 / 3; 
+    .introduction-wrapper div h2 {
+      display: flex;
+      align-items: center;
     }
-    .div3 { grid-area: 1 / 1 / 2 / 3; }
-    .div4 { grid-area: 3 / 1 / 4 / 3; }
+    .center {
+      grid-area: 2 / 2 / 3 / 3;
+      text-align: center;
+      min-width: fit-content;
+    }
+    .left {
+        grid-area: 1 / 1 / 2 / 2;
+    }
+    .bottom-left {
+        grid-area: 3 / 1 / 4 / 2;
+    }
+    .bottom-right {
+        grid-area: 3 / 3 / 4 / 4;
+    }
+    .right {
+        grid-area: 1 / 3 / 2 / 4;
+    }
 
-    h1 {
-        font-size: 10em;
-        font-weight: 500;
-        margin-bottom: 0px;
-    }
-    .content {
-        display: flex;
-        flex-direction: row;
-        width: 100vw;
-        min-height: 100vh;
-        height: 100%;
+    @media(orientation:portrait) {
+      .introduction-wrapper {
+        display:flex !important;
+        flex-direction: column;
+      }
+      .introduction-wrapper div {
+        margin: 2em;
+      }
     }
 
-    @media(max-width: 1330px) {
-
-
-    } 
-    @media(max-width: 1030px) {
-        h1 {
-            font-size: 7em;
+    @keyframes textHue {
+        0% {
+            background-position: 0 50%;
+        } 50% {
+            background-position: 100% 0;
+        } 100% {
+            background-position: 0 -50%;
         }
-        h2 {
-            font-size: 5em;
-        }
-        h3 {
-            font-size: 1.5em;
-        }
-    }
-    @media (orientation: portrait), (max-width: 870px) {
-        .parent {
-            margin-top: auto;
-            margin-bottom: auto;
-            grid-template-columns: 1fr;
-            grid-template-rows: auto;
-        } 
-        .parent * {
-            align-items: center;
-            margin-right: auto;
-            margin-left: auto;
-        }
-        .button-holder x {
-            width: 45%;
-            height: 4em;
-        }
-        .button-holder x button a {
-            font-size: 1.25em !important;
-            font-weight: 500;
-        }
-        .button-holder x:last-child {
-            margin-left: 4px;
-        }
-        .button-holder x:first-child {
-            margin-right: 4px;
-        }
-        .info {
-            font-weight: 300;
-            margin-left: 2em;
-            margin-right: 2em;
-        }  
-        .parent div {
-            grid-area: auto/auto/auto/auto;
-            width: 100vw;
-            height: fit-content;
-            
-        }
-        .parent div * {
-            margin-left: auto;
-            margin-right: auto;
-        }
-        h1 {
-            font-size: 12em;
-        }
-        .text-container {
-            text-align: center;
-        }
-        .info {
-            font-weight: 500;
-            margin-right: 2em !important;
-            margin-left: 2em !important;
-            max-width: 80ch;
-        }   
-    }
-    @media(max-width: 795px), (orientation: landscape) {
-        h1 {
-            font-size: 10em;
-        }
-        h2 {
-            font-size: 8em;
-        }
-    }
-    @media(max-width: 690px) {
-        h1 {
-            font-size: 8em;
-        }
-        h2 {
-            font-size: 6em;
-        }
-    }
-    @media(max-width: 450px) {
-        h1 {
-            font-size: 7em;
-        }
-        h2 {
-            font-size: 5em;
-        }
-        .button-holder {
-            width: 100%;
-        }
-        .info {
-            margin-right: 1em !important;
-            margin-left: 1em !important;
-        }   
-    }
-    @media(max-width: 300px) {
-        h1 {
-            font-size: 5em;
-        }
-        h2 {
-            font-size: 3em;
-        }
-        h2 {
-            font-size: 4em;
-        }
-    }
-    .info {
-        font-weight: 500;
-        margin-right: 10vw;
-        max-width: 80ch;
-    }   
-
-    h2 {
-        font-size: 6em;
-        max-width: 90vw;
-        overflow-wrap: break-word;
-    }
-    h3 {
-        font-size: 2em;
     }
 </style>
