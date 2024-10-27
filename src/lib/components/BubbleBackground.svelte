437838dd8dd0b7ce70ad6fb01a5edf0f190f0401
ctih1 <onni09nevala@gmail.com>
2024-10-27 17:28:16 +0200

    I stole halloween

--- src/lib/components/BubbleBackground.svelte
+++ src/lib/components/BubbleBackground.svelte
@@ -37,10 +37,9 @@
         transition: all 15s linear;
         aspect-ratio: 1;
         position: absolute;
-        background-image: url("/pumpkin.png");
-        background-size: 100% 100%;
+        background-color: var(--primary);
         border-radius: 100%;
-        opacity: 1;
+        opacity: 0.02;
         z-index: 0;
     }
     .circles {
