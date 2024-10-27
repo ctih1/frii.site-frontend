1c8a35ae68b70d851878f17283a2349693171dff
ctih1 <onni09nevala@gmail.com>
2024-10-27 18:52:04 +0200

    added ability to discard banner

--- src/lib/components/Banner.svelte
+++ src/lib/components/Banner.svelte
@@ -5,6 +5,7 @@
     let height:number;
     let loaded:boolean=false;
     let danger:boolean=false;
+
     let message:string;
     getStatus().then(response=> {
         if(response.status===204) {
@@ -15,22 +16,33 @@
                 console.log(data);
                 danger=true;
                 message=data;
+                calcIsHidden();
                 loaded=true;
             })
         }})
       loaded=true;
 
     let hidden:boolean = false;
-    if(browser) {
-      hidden = (localStorage.getItem("notification-hidden")??false) as boolean;
+
+    function calcIsHidden():boolean {
+      if(!browser) { return false; }
+      hidden = (localStorage.getItem("notification-hidden")??false) as boolean && localStorage.getItem("notification-hidden-message") === message;
+      return hidden;
     }
 </script>
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-{#if loaded && danger}
+{#if loaded && danger && !hidden}
     <div bind:clientHeight={height} class="bar">
         <span style="margin-left: 1em;" class="material-symbols-outlined">warning</span>
         <p>{message}</p>
+
+        <a on:click={()=>{
+          localStorage.setItem("notification-hidden","true");
+          localStorage.setItem("notification-hidden-message", message);
+          hidden=true;
+        }}>X</a>
+
     </div>
     <div style="height: {height}px" class="pusher"></div>
 {/if}
@@ -47,5 +59,11 @@
     }
     .bar * {
         color: white;
+    }
+    a {
+        cursor: pointer;
+        margin-left: auto;
+        margin-right: 2em;
+        font-size: 1em;
     }
 </style>
