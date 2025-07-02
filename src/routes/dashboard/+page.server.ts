7cdb2f653a20b6309c0c4c77c599cd8fc7f35dcd
ctih1 <nevalaonni@gmail.com>
2025-07-02 18:31:23 +0300

    fixed 500 on dashboard if cookie not set

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,5 +1,5 @@
 export async function load({ cookies }) {
 	return {
-		domainAmount: Number(cookies.get("domain-amount")) ?? 3
+		domainAmount: Number(cookies.get("domain-amount") ?? 3) ?? 3
 	};
 }
