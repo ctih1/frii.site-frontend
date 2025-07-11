81fe227370c92fa2630e383bdbba5547fefe1c03
ctih1 <nevalaonni@gmail.com>
2025-07-11 15:09:46 +0300

    Report page redone

--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -1,21 +1,16 @@
 <script lang="ts">
-	import Holder from "$lib/components/Holder.svelte";
 </script>
 
-<Holder>
-	<h1>What do you want to report?</h1>
-	<div class="options">
+<div class="bg-card mt-8 mr-auto ml-auto w-11/12 max-w-4xl rounded-xl p-4">
+	<h1 class="text-2xl font-semibold">What do you want to report?</h1>
+	<div class="options flex flex-col">
 		<a href="https://forms.gle/Lyi54cdLb5dUNnyU8">Security vulnerability</a>
 		<a href="https://github.com/ctih1/frii.site-frontend/issues/new">A bug</a>
 		<a href="https://forms.gle/Ct7fLpkMHSk2zqJdA">Abuse</a>
 	</div>
-</Holder>
+</div>
 
 <style>
-	.options {
-		display: flex;
-		flex-direction: column;
-	}
 	.options * {
 		margin-top: 1em;
 	}
