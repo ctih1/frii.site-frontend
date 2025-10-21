35946ece8a34eb943a7bbde7d7dc62fc201ac027
ctih1 <nevalaonni@gmail.com>
2025-10-21 17:55:48 +0300

    replaced turnstile page with plain html

---
+++ src/routes/turnstile/+server.ts
@@ -0,0 +1,39 @@
+const html = `
+<head>
+	<link
+		rel="preload"
+		href="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit"
+		as="script" />
+	<script src="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit"></script>
+</head>
+
+<div style="position: absolute; top: 0px; left: 0px" id="turnstile-container"></div>
+
+<script>
+    turnstile.ready(function () {
+        let container = document.getElementById("turnstile-container");
+
+        if (container) {
+            container.innerHTML = "";
+        }
+        // @ts-ignore
+        widgetId = turnstile.render("#turnstile-container", {
+            sitekey: "0x4AAAAAABiGbbOhSUc5vWl9",
+            callback: function (token) {
+                if (window.webkit) {
+                    window.webkit?.messageHandlers.turnstile.postMessage(token);
+                }
+            }
+        });
+    });
+</script>
+
+`;
+
+export function GET() {
+	return new Response(html, {
+		headers: {
+			"Content-Type": "text/html"
+		}
+	});
+}
