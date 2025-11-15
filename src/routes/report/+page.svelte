cc04da79638e66cd6f191e306cb556d98839b1f7
WhatDidYouExpect <webmaster@expect.ovh>
2025-11-15 13:49:48 +0100

    translation of report page

--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
+	import { m } from "../../paraglide/messages";
 </script>
 
 <div class="bg-card mt-8 mr-auto ml-auto w-11/12 max-w-4xl rounded-xl p-4">
-	<h1 class="text-2xl font-semibold">What do you want to report?</h1>
+	<h1 class="text-2xl font-semibold">{m.security_report_what_would_you_like_to_report()}</h1>
 	<div class="options flex flex-col">
-		<a href="https://forms.gle/Lyi54cdLb5dUNnyU8">Security vulnerability</a>
-		<a href="https://github.com/ctih1/frii.site-frontend/issues/new">A bug</a>
-		<a href="https://forms.gle/Ct7fLpkMHSk2zqJdA">Abuse</a>
+		<a href="https://forms.gle/Lyi54cdLb5dUNnyU8">{m.security_report_vulnerability()}</a>
+		<a href="https://github.com/ctih1/frii.site-frontend/issues/new">{m.security_report_bug()}</a>
+		<a href="https://forms.gle/Ct7fLpkMHSk2zqJdA">{m.security_report_abuse()}</a>
 	</div>
 </div>
 
